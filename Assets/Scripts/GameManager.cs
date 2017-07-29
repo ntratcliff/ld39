b9e1cd126ea9634d656399bf32d6fa1d32732960
Noah Ratcliff <me@nratcliff.com>
2017-07-29 14:24:40 -0400

    import assets, setup scene, player movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+    public static GameManager Instance;
+
+    public Camera MainCamera;
+    public Camera RTCamera;
+
+    public PlayerController Player;
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    public float MovementForce;
+    public float MaxSpeed;
+
+    public HingeJoint2D RearWheel, FrontWheel;
+
+    // attached components
+    private Rigidbody2D _body;
+
+    // movement variables
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    // apply forces in fixedupdate
+    private void FixedUpdate()
+    {
+        _updateMovement();
+    }
+
+    private void _updateMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if(horizontal != 0)
+        {
+            _body.AddForce(transform.right * Mathf.Sign(horizontal) * MovementForce);
+            _clampSpeed();
+        }
+
+        // apply brakes when not moving
+        RearWheel.useMotor = horizontal == 0;
+        FrontWheel.useMotor = horizontal == 0;
+    }
+
+    private void _clampSpeed()
+    {
+        Vector2 localVelo = transform.InverseTransformDirection(_body.velocity);
+        localVelo.x = Mathf.Clamp(localVelo.x, -MaxSpeed, MaxSpeed);
+        _body.velocity = localVelo;
+    }
+}
